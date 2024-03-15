287e64aff69bb908b1bcd1b4e3c57eaa9e4a6eb6
Antonyvijay19 <vijayantony07@gmail.com>
2024-03-16 02:50:16 +0530

    completed with react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./style.css"
 
+
+function Imagegallery(){
+  return(
+    <div className="imagecontainer">
+    
+     <Images Details={details}></Images>
+    </div>
+  )
+}
+
+function Images({Details}){
+     return(
+      <>
+   {
+      Details.map(function(item){
+        return(
+          <div className="photos">
+            <img src={item.image} alt=""></img>
+            <h2>{item.tag}</h2>
+          </div>
+
+        );
+      })}
+      </>  
+     )}
+
+var details =[
+  {
+    image:"./Assets/images/1.jpg",
+    tag: "Julies Rabbit Ears"
+  },
+  {
+    image:"./Assets/images/2.jpg",
+    tag: "The innocent look"
+  },
+  {
+    image:"./Assets/images/3.jpg",
+    tag: "big eyed pugg"
+  },
+  {
+    image:"./Assets/images/4.jpg",
+    tag: "The saint Doggo"
+  },
+  {
+    image:"./Assets/images/3.jpg",
+    tag: "big eyed pugg"
+  },
+  {
+    image:"./Assets/images/2.jpg",
+    tag: "The innocent look"
+  },
+ 
+  {
+    image:"./Assets/images/4.jpg",
+    tag: "The saint Doggo"
+  },
+  {
+    image:"./Assets/images/1.jpg",
+    tag: "Julies Rabbit Ears"
+  }
+]
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+   <Imagegallery></Imagegallery>
+   </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
